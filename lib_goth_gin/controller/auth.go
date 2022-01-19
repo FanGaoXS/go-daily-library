f1fb1dd57e625a43f38d13f26873c4d77d64b3cf
FanGaoXS <954278478@qq.com>
2022-01-19 18:16:45 +0800

    lib@wqk 完成controller中对provider的注释

--- lib_goth_gin/controller/auth.go
+++ lib_goth_gin/controller/auth.go
@@ -8,12 +8,13 @@ import (
 )
 
 func BeginAuth(c *gin.Context) {
-	// 前往验证（会自动从url中解析provider的值）
+	// 前往验证（会自动从request中解析provider的值，然后跳转到对应的provider供应商进行验证）
 	gothic.BeginAuthHandler(c.Writer, c.Request)
 }
 
 func AuthCallBack(c *gin.Context) {
 	provider := c.Param("provider")
+	// 会自动解析来自provider供应商的回调返回的user
 	user, err := gothic.CompleteUserAuth(c.Writer, c.Request)
 	if err != nil {
 		fmt.Fprintln(c.Writer, err)
@@ -26,6 +27,7 @@ func AuthCallBack(c *gin.Context) {
 
 func Logout(c *gin.Context) {
 	provider := c.Param("provider")
+	// 会自动从request解析provider的值，然后对对应的provider供应商进行logout
 	err := gothic.Logout(c.Writer, c.Request)
 	if err != nil {
 		c.JSON(http.StatusOK, gin.H{
