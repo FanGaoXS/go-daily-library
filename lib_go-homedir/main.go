34cbf73ae008edcd411c1b2adba4c2c911cf9f76
qingke.wu <a954278478@gmail.com>
2022-01-27 18:40:39 +0800

    add go-homedir

--- lib_go-homedir/main.go
+++ lib_go-homedir/main.go
@@ -12,4 +12,12 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("Home dir:", dir)
+
+	dir = "~/golang/src"
+	expandedDir, err := homedir.Expand(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Expand of %s is: %s\n", dir, expandedDir)
 }
